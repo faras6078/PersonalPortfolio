50c8db766544e9ba442f683db3b1aadbe7540ebd
Faras Lambade <faras6078@gmail.com>
2022-09-26 19:35:04 +0530

    Personal portfolio
    website to show case my skills and to get in touch

---
+++ js/script.js
@@ -0,0 +1,22 @@
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+
+menu.addEventListener("click", function () {
+    navbar.classList.toggle("active");
+});
+
+Window.onscroll = () => {
+    navbar.classList.remove("active");
+}
+
+const checkbox =
+
+document.getElementById('checkbox');
+
+checkbox.addEventListener('change',  () => {
+        
+        document.body.classList.toggle('dark'); 
+    });
+
+    
